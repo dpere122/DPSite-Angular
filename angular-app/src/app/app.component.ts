ef986e24d90381a472a26cb643697270870fd0ff
Daniel <dpere381@fiu.edu>
2021-08-28 15:13:38 -0400

    Got Scroll event to work

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 
 import{
   trigger,
@@ -7,32 +7,32 @@ import{
   animate,
   transition
 } from '@angular/animations';
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
   animations:[
     trigger('navAnimationState',[
-      state('show',style({
-        opacity: 1
+      state('max',style({
+        transform: 'scaleY(1.2)',
       })),
-      state('hide',style({
-        opacity: 0
+      state('min',style({
+        transform: 'scaleY(1)',
       })),
-      transition('show => hide', animate('600ms ease-out')),
-      transition('hide => show', animate('1000ms ease-in'))
+      transition('max => min', animate('600ms ease-out')),
+      transition('min => max', animate('1000ms ease-in'))
     ])
   ]
 })
 export class AppComponent {
   show = false;
   constructor (){}
+
   get stateName(){
-    return this.show ? 'show' :'hide';
+    return this.show ? 'max' :'min';
   }
-  toggle(){
-    this.show = !this.show;
+  @HostListener('window:scroll', ['$event'])
+  onScrollEvent(){
+    console.log("Scrolled");
   }
 }
